fc8688d91ffb2e9df5da9f5886286a77c502633a
Indra Eka Prasetya <indraep92@gmail.com>
2014-01-01 13:57:08 +0700

    change listview implementation using custom arrayadapter

---
+++ src/com/example/adapter/CustomArrayAdapter.java
@@ -0,0 +1,39 @@
+package com.example.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.model.Person;
+import com.example.sqlite_examplev2.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomArrayAdapter extends ArrayAdapter<Person> {
+	private final Activity context;
+	private final List<Person> people;
+
+	public CustomArrayAdapter(Activity context, List<Person> people) {
+		super(context, R.layout.rowlayout, people);
+		this.context = context;
+		this.people = people;
+	}
+
+	public View getView(int pos, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View rowView = inflater.inflate(R.layout.rowlayout, parent, false);
+		TextView textView = (TextView) rowView.findViewById(R.id.secondLine);
+		ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+		textView.setText(people.get(pos).toString());
+		
+		imageView.setImageResource(R.drawable.ic_launcher);
+		
+		return rowView;
+	}
+}
--- src/com/example/sqlite_examplev2/PersonListActivity.java
+++ src/com/example/sqlite_examplev2/PersonListActivity.java
@@ -2,20 +2,25 @@ package com.example.sqlite_examplev2;
 
 import java.util.List;
 
+import com.example.adapter.CustomArrayAdapter;
 import com.example.helper.DatabaseHelper;
 import com.example.model.Person;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class PersonListActivity extends ListActivity {
+public class PersonListActivity extends Activity {
 
 	DatabaseHelper db;
+	ListView listview;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +28,19 @@ public class PersonListActivity extends ListActivity {
 		setContentView(R.layout.activity_person_list);
 
 		db = DatabaseHelper.getInstance(getApplicationContext());
+		listview = (ListView)findViewById(R.id.listview);
 		
 		List<Person> people = db.getAllPeople();
-		Person[] values = people.toArray(new Person[people.size()]);
 		
-		ArrayAdapter<Person> adapter = new ArrayAdapter<Person>(this,
-				android.R.layout.simple_list_item_1, values);
-		setListAdapter(adapter);
+		CustomArrayAdapter adapter = new CustomArrayAdapter(this, people);
+		listview.setAdapter(adapter);
+		
+		listview.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Toast.makeText(getApplicationContext(), "position = " + position, Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 
 	@Override
